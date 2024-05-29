036879da2056212358106ba8c39ab34ed8f9f62e
Oleg Diachenko <diachenko.oleg@proton.me>
2024-05-29 21:49:02 +0300

    Second commit

--- filter.py
+++ filter.py
@@ -20,21 +20,23 @@ def check_company_name(text):
     else:
         return '?'
 
-# Подключение к базе данных
-connection = sqlite3.connect('news_data.db')
-connection.row_factory = sqlite3.Row  # Позволяет обращаться к столбцам по имени
-cursor = connection.cursor()
 
-# Получение строк с NULL значением в столбце company
-cursor.execute("SELECT rowid, text FROM news WHERE company IS NULL")
-rows = cursor.fetchall()
+def filter_pls():
+    # Подключение к базе данных
+    connection = sqlite3.connect('news_data.db')
+    connection.row_factory = sqlite3.Row  # Позволяет обращаться к столбцам по имени
+    cursor = connection.cursor()
 
-# Применение функции check_company_name к каждой строке и обновление базы данных
-for row in rows:
-    text = row['text']  # замените 'text' на имя вашего столбца с текстом
-    company_result = check_company_name(text)
-    cursor.execute("UPDATE news SET company = ? WHERE rowid = ?", (company_result, row['rowid']))
+    # Получение строк с NULL значением в столбце company
+    cursor.execute("SELECT rowid, text FROM news WHERE company IS NULL")
+    rows = cursor.fetchall()
 
-# Завершение и сохранение изменений
-connection.commit()
-connection.close()
+    # Применение функции check_company_name к каждой строке и обновление базы данных
+    for row in rows:
+        text = row['text']  # замените 'text' на имя вашего столбца с текстом
+        company_result = check_company_name(text)
+        cursor.execute("UPDATE news SET company = ? WHERE rowid = ?", (company_result, row['rowid']))
+
+    # Завершение и сохранение изменений
+    connection.commit()
+    connection.close()
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 from parsing import parse_pls
+from filter import filter_pls
+from pnd import save_pls
 
 
 def main():
     parse_pls()
+    filter_pls()
+    save_pls()
 
 
 if __name__ == "__main__":
